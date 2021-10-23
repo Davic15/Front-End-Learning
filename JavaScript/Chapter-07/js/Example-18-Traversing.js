0b839ab83cdcedae755fa848f1be511a413077b1
Franklin David Macias Avellan <franklin.macias864@gmail.com>
2021-10-23 18:38:06 +0200

    Chapter 7 (html + css + js + jquery)

---
+++ JavaScript/Chapter-07/js/Example-18-Traversing.js
@@ -0,0 +1,13 @@
+$(function() {
+    var $h2 = $('h2');
+    $('ul').hide();
+    $h2.append('<a class="show">show</a>');
+
+    $h2.on('click', function() {
+        $h2.next('ul')
+            .fadeIn(500)
+            .children('.hot')
+            .addClass('complete');
+        $h2.find('a').fadeOut();
+    });
+});
