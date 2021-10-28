351d428bd58cb163f81db70279e3d8aab94d587f
Franklin David Macias Avellan <franklin.macias864@gmail.com>
2021-10-28 12:34:10 +0200

    Chapter 9 (images + css + jquery + html + js)

---
+++ JavaScript/Chapter-09/js/Example-03-Session-Storage.js
@@ -0,0 +1,15 @@
+if(Modernizr.sessionstorage) {
+    var txtUsername = document.getElementById('username'),
+        txtAnswer = document.getElementById('answer');
+    
+    txtUsername.value = sessionStorage.getItem('username');
+    txtAnswer.value = sessionStorage.getItem('answer');
+
+    txtUsername.addEventListener('input', function() {
+        sessionStorage.setItem('username', txtUsername.value);
+    }, false);
+
+    txtAnswer.addEventListener('input', function() {
+        sessionStorage.setItem('answer', txtAnswer.value);
+    }, false);
+}
