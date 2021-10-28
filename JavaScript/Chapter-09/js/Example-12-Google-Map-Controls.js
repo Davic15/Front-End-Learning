8e2fe0d8f11ba9ae97e8ada6c86ff4b4d89adc06
Franklin David Macias Avellan <franklin.macias864@gmail.com>
2021-10-28 13:57:40 +0200

    Chapter 9 (images + css + jquery + html + js)

---
+++ JavaScript/Chapter-09/js/Example-12-Google-Map-Controls.js
@@ -0,0 +1,34 @@
+function init() {
+    var mapOptions = {
+        zoom: 14,
+        center: new google.maps.LatLng(40.782710, -73.965610),
+        panControl: false,
+        zoomControl: true,
+        zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.SMALL,
+        position: google.maps.ControlPosition.TOP_RIGHT
+    },
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+      position: google.maps.ControlPosition.TOP_LEFT
+    },
+        scaleControl: true,
+        scaleControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER
+    },
+        streetViewControl: false,
+        overviewMapControl: false
+  };
+  var venueMap = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
+
+
+function loadScript() {
+    var script = document.createElement('script');
+    script.src = 'http://maps.googleapis.com/maps/api/js?sensor=false&callback=init';
+    document.body.appendChild(script);
+}
+
+
+window.onload = loadScript;
