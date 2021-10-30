07ab85f2c448bc5c5a0839711e48b3dcb9d3d4f5
Franklin David Macias Avellan <franklin.macias864@gmail.com>
2021-10-30 16:22:34 +0200

    Chapter 12 (css + images + html + jquery + js)

---
+++ JavaScript/Chapter-12/js/Example-02-Filter-Filter.js
@@ -0,0 +1,37 @@
+$(function() {
+    var people = [
+        {                                             
+            name: 'Casey',                               
+            rate: 60
+        },
+        {
+            name: 'Camille',
+            rate: 80
+        },
+        {
+            name: 'Gordon',
+            rate: 75
+        },
+        {
+            name: 'Nigel',
+            rate: 120
+        }
+    ];
+
+    function priceRange(person) {
+        return(person.rate >= 65) && (person.rate <= 90);
+    };
+
+    var results = [];
+    results = people.filter(priceRange);
+
+    var $tableBody = $('<tbody></tbody>');
+    for (var i = 0; i < results.length; i++) {
+        var person = results[i];
+        var $row = $('<tr></tr>');
+        $row.append($('<td></td>').text(person.name));
+        $row.append($('<td></td>').text(person.rate));
+        $tableBody.append($row);
+    }
+    $('thead').after($tableBody);
+});
