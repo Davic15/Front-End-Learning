f8c4d824f694cecb1c62851777dc04c8704c6e1e
Franklin David Macias Avellan <franklin.macias864@gmail.com>
2021-11-05 14:00:20 +0100

    Eloquent JavaScript - Chapter 4

---
+++ EloquentJavaScript/Chapter-04/js/Example-02-Reversing-Array.js
@@ -0,0 +1,25 @@
+function reverseArray(array) {
+    let newArray = [];
+    for (let i = array.length -1; i >= 0; i--){
+        newArray.push(array[i]);
+    }
+    return newArray;
+}
+
+function reverseArrayInPlace(array) {
+    for (let i = 0; i < Math.floor(array.length / 2); i++){
+        let old = array[i];
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = old;
+    }
+    return array;
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+
+
+let array = [4,3,7,8,1,3,10];
+console.log(reverseArray(array));
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
